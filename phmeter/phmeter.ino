5c945261486a41095776f8419f65e70c8628d88c
Pablo <pablo.gomezgonzalo@gmail.com>
2019-01-29 13:46:50 +0100

    First working version!!
    Not finishied, but usefull version.

--- phmeter/phmeter.ino
+++ phmeter/phmeter.ino
@@ -8,7 +8,7 @@ const int pHAnPin[4] = {A0, A1, A2, A3};  // Each pH-meter has an analog imput.
 const int pHDigPin[4] = {2,3,4,5};
 
 // Variables for operation
-float pH_Control[4]; // Target pH value. An electrovalve should be open if needed.
+float pH_Control[4] = {6,6,6,6}; // Target pH value. An electrovalve should be open if needed.
 float m[4];   // pH = m * (measure) + n
 float n[4];
 float pH_Value[4]; //pH value: from 0[-] to 14[-]
@@ -46,62 +46,73 @@ while(phmeters_number > 4 || phmeters_number < 1)
 InizialicePHmeters(phmeters_number, pHDigPin); // Set the pins in digital, low mode.
 
 for (int i=0; i<phmeters_number; i++){
+// ---------------------------
+//  NOTA: NO ESTAMOS INCLUYENDO 
+//  ESTABILIZACIÓN DE LOS PUNTOS
+// ---------------------------
+  
   Serial.println((String)"Calibration of pHmeter number "+i+" :");
   Serial.print("Enter first calibration pH: ");
   Patron_1_pH = Serial.parseFloat();
-  pH_Digital = ReadPh(pHDigPin[i]) 
+  pH_Digital = ReadPh(pHAnPin[i]) ;
   Patron_1_V = 5*pH_Digital/1024.0;
-  
-  //while (Serial.available() == 0);
-  //Patron_1_pH = Serial.parseFloat();
-  //Serial.println(Patron_1_pH);
-  //pH_Digital = analogRead(pHSensorPin);
-  //Patron_1_V = 5*pH_Digital/1024.0;
-  //delay(500);
 
   Serial.print("Enter second calibration pH: ");
   while (Serial.available() == 0);
   Patron_2_pH = Serial.parseFloat(); 
-  pH_Digital = ReadPh(pHDigPin[i])
+  pH_Digital = ReadPh(pHAnPin[i]);
   Patron_2_V = 5*pH_Digital/1024.0;
   Serial.println(Patron_2_pH);
   delay(500);
   
-  
-  //
-  //   INTRODUCIR CALCULO DE M Y N
-  //
+  m[i] = (Patron_2_pH - Patron_1_pH)/(Patron_2_V - Patron_1_V);
+  n[i] = Patron_1_pH-m[i]*Patron_1_V;
 }
 }
 
 void loop() {
 
-pH_Digital = analogRead(pHSensorPin);
-pH_Voltage = 5*pH_Digital/1024.0;
-m = (Patron_2_pH - Patron_1_pH)/(Patron_2_V - Patron_1_V);
-n = Patron_1_pH-m*Patron_1_V;
-pH_Value = pH_Voltage*m + n;
+for (int i=0; i<phmeters_number; i++){
+  pH_Value[i]=m[i]*ReadPh(pHAnPin[i])+n[i];
+  Serial.print(pH_Value[i]);
+}
+// Serial.print(pH_Value);
 
-Serial.print("pH Digital Read: ");
-Serial.println(pH_Digital);
-delay(500);
-Serial.print("pH Voltage: ");
-Serial.println(pH_Voltage, 2);
-delay(500);
-Serial.print("pH Value: ");
-Serial.println(pH_Value, 2);
-Serial.println(" ");
-
-if (pH_Value > pH_Control){
-  digitalWrite(2, HIGH);
-  Serial.println(" -- Readjusting pH -- ");
-  Serial.println(" ");
-  delay(250);
-  digitalWrite(2, LOW);
-  delay(2000);
+for (int i=0; i<phmeters_number; i++){
+  if(pH_Value[i]>pH_Control[i]){
+    digitalWrite(pHDigPin[i], HIGH);
+    delay(1500);       // miliseconds the electrovalve should be open to correct pH. 
+    digitalWrite(pHDigPin[i], LOW);
+  }
 }
 
-delay(2000);
+delay(10000); // Time to wait between measures
+//pH_Digital = analogRead(pHSensorPin);
+//pH_Voltage = 5*pH_Digital/1024.0;
+//m = (Patron_2_pH - Patron_1_pH)/(Patron_2_V - Patron_1_V);
+//n = Patron_1_pH-m*Patron_1_V;
+//pH_Value = pH_Voltage*m + n;
+
+//Serial.print("pH Digital Read: ");
+//Serial.println(pH_Digital);
+//delay(500);
+//Serial.print("pH Voltage: ");
+//Serial.println(pH_Voltage, 2);
+//delay(500);
+//Serial.print("pH Value: ");
+//Serial.println(pH_Value, 2);
+//Serial.println(" ");
+
+//if (pH_Value > pH_Control){
+//  digitalWrite(2, HIGH);
+//  Serial.println(" -- Readjusting pH -- ");
+//  Serial.println(" ");
+//  delay(250);
+//  digitalWrite(2, LOW);
+//  delay(2000);
+//}
+
+//delay(2000);
 
 }
 
@@ -120,5 +131,5 @@ void InizialicePHmeters(int number, int pin[4]){ // Inizialice the required numb
     temp += analogRead(pin);
     delay(200);
   }
-  return(temp/5)
+  return(temp/5);
 }
